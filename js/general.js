ac213fcecf0c55a41c35ec73e88bac5226b0125c
sathyamshu2357 <sathyamshu2357@gmail.com>
2020-12-29 23:37:45 +0530

    image slider

--- js/general.js
+++ js/general.js
@@ -13,3 +13,22 @@ function changeCss(value) {
     if (value == 0) { document.getElementById('csssheet').href = "css/desktop.css"; console.log("desktop view"); }
     else if (value == 1) { document.getElementById('csssheet').href = "css/mobile.css"; console.log("mobile view"); }
 }
+
+var pics = ["assets/slider1.jpg","assets/slider2.jpg","assets/slider3.jpg"]
+var idx  = 0;
+
+var intervals = function slideshow() {
+    console.log("came here..");
+    setInterval(function () {
+    console.log(pics[idx]);
+    document.getElementById("sliderid").src = pics[idx];
+    idx += 1;
+    idx %= 3;   
+},3000)
+}
+
+function changeSlide(val) {
+    clearInterval(intervals);
+    idx = val;
+    intervals;    
+}
